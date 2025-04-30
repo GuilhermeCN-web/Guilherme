4e11add2198bd5ddf2a0e562a5acd0c8301714d7
LukEscopeta <lfcnarcizo73@gmail.com>
2025-04-30 16:21:49 -0300

    criacao das outras paginas e correçao de bugs de links

---
+++ quiz 3/Scripts/script10.js
@@ -0,0 +1,36 @@
+document.addEventListener("keydown", function(event) {
+    if (event.ctrlKey && (event.key === "u" || event.key === "U")) {
+        event.preventDefault(); 
+        window.location.href = "https://www.youtube.com/watch?v=f_k4gBdRXKA"; 
+    }
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const respostaCorreta = "resposta-c"; 
+    const proximaPergunta = "pergunta11.html"; 
+    const indexPagina = "../Index.html"; 
+
+    const botoes = document.querySelectorAll(".resposta");
+
+    botoes.forEach(botao => {
+        botao.addEventListener("click", function () {
+            botoes.forEach(c => c.disabled = true); 
+
+            if (this.id === respostaCorreta) {
+                this.style.backgroundColor = "green";
+                exibirAlerta("✅ Resposta correta!", proximaPergunta);
+            } else {
+                this.style.backgroundColor = "red";
+                exibirAlerta("❌ Resposta errada! Voltando ao início...", indexPagina);
+            }
+        });
+    });
+
+    function exibirAlerta(mensagem, link) {
+        setTimeout(() => {
+            alert(mensagem);
+            window.location.href = link;
+        }, 500); 
+    }
+});
